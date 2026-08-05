d2925ee3ffa9ae1a883f400f0bbc962e3bd11ce5
BirDan1988 <birjudanak@gmail.com>
2026-08-05 10:38:51 +0530

    hot series selection

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     loop:true,
     margin:10,
     nav:false,
+    dots:true,
     autoplay: true,
     autoplayTimeout: 4000, // wait 4 seconds before next slide
     autoplaySpeed: 2000,   // slide movement takes 1 second
@@ -22,9 +23,9 @@ $(document).ready(function(){
     }
 })
 
-   var newsSlider = $('.news-slider');
+   let storySlider = $('.story-slider');
 
-newsSlider.owlCarousel({
+storySlider.owlCarousel({
     loop:true,
     margin:10,
     nav:false,
@@ -48,13 +49,53 @@ newsSlider.owlCarousel({
 });
 
 $('.button-right').click(function () {
-    newsSlider.trigger('prev.owl.carousel');
+    storySlider.trigger('prev.owl.carousel');
 });
 
 $('.button-left').click(function () {
+    storySlider.trigger('next.owl.carousel');
+});
+
+
+let newsSlider = $('.news-slider');
+
+newsSlider.owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+
+    autoplay:false,
+    autoplayTimeout:4000,
+    autoplaySpeed:2000,
+    autoplayHoverPause:true,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        640:{
+            items:2
+        },
+        1024:{
+            items:4
+        }
+    }
+});
+
+$('.news-button-right').click(function () {
+    newsSlider.trigger('prev.owl.carousel');
+});
+
+$('.news-button-left').click(function () {
     newsSlider.trigger('next.owl.carousel');
 });
 
+$('#series ul li a').click(function(){
+    $(this).addClass('text-white  bg-blue-500 rounded-2xl block')
+    $(this).parent().siblings().find('a').removeClass('text-white bg-blue-500')
+    $(this).parent().siblings().find('a').addClass('text-[#A0B3C5]')
+
+})
 
 
 
